53d9c48282b5b30fcc759d807c34c57273e8bbf8
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-29 17:26:58 +0000

    chore: add opaque type utility

--- packages/fastify/_test/dummy.test.ts
+++ packages/fastify/_test/dummy.test.ts
@@ -1,13 +1,15 @@
+const Server_ = Fastify.fastify()
+interface Server extends Fastify.GetFastifyInstanceType<typeof Server_> {}
+const Server = Fastify.opaque(Server_)<Server>()
+
 describe("Dummy", () => {
   it("dummy", async () => {
-    const F = fastify()
-
     const program = Do(($) => {
-      const addr = $(F.listen({ port: 40001, host: "0.0.0.0" }))
+      const addr = $(Server.listen({ port: 40001, host: "0.0.0.0" }))
       return addr
     })
 
-    const res = await program.scoped().provideSomeLayer(F.Live).unsafeRunPromise()
+    const res = await program.scoped().provideSomeLayer(Server.Live).unsafeRunPromise()
 
     assert.strictEqual(res, "http://0.0.0.0:40001")
   })
